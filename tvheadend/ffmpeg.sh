6b9de31d28c1041a1d164078be409610771cf934
toleeck <40557618+toleeck@users.noreply.github.com>
2019-10-07 14:25:18 +0300

    Update and rename sed shara.club ffmpeg.txt to ffmpeg.sh

---
+++ tvheadend/ffmpeg.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#link to iptv-playlist
+URL="https://"
+#temp for playlist
+M3U="/tmp/playlist.m3u"
+#location for a new playlist with ffmpeg pipe
+M3U_FFMPEG="/home/hts/.hts/tvheadend/m3u/ffmpeg.m3u"
+
+#search text. there will be a pipe in this line. e.g. part of the url from the media stream
+STRING=""
+#start of ffmpeg pipe
+START="pipe:\/\/\/usr\/bin\/ffmpeg -loglevel quiet -i "
+#end of ffmpeg pipe
+END=" -c copy -tune zerolatency -flags +global_header -strict -2 -f mpegts pipe:1"
+
+#download iptv-playlist
+wget -O $M3U $URL
+#adding a pipe for lines with links to media streams
+sed "/${STRING}/s/^/${START}/;/${STRING}/s/$/${END}/;s/\r//g" $M3U > $M3U_FFMPEG
+
+chown hts:video $M3U_FFMPEG
